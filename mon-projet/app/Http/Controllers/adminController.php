0cd851cf9db2e8928a6c8f2bd3bde61c2d3774bf
celia9815 <celiacici430@gmail.com>
2026-01-12 16:35:51 +0100

    test

--- mon-projet/app/Http/Controllers/adminController.php
+++ mon-projet/app/Http/Controllers/adminController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth; 
+
+// celia  
 
 class AdminController extends Controller
 {
